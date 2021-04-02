aca4ef47864c87622275f37d1cbb7be755eefbc8
ruijadom <ruijadomingues@gmail.com>
2021-04-02 14:45:11 +0100

    cloudinary create image signature

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
-// import { useMutation, gql } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
+
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -13,7 +14,40 @@ import { SearchBox } from "./searchBox";
 //   UpdateHouseMutation,
 //   UpdateHouseMutationVariables,
 // } from "src/generated/UpdateHouseMutation";
-// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+
+const SIGNATURE_MUTATION = gql`
+  mutation CreateSignatureMutation {
+    createImageSignature {
+      signature
+      timestamp
+    }
+  }
+`;
+
+interface IUploadImageResponse {
+  secure_url: string;
+}
+
+async function uploadImage(
+  image: File,
+  signature: string,
+  timestamp: number
+): Promise<IUploadImageResponse> {
+  const url = `	https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`;
+  const formData = new FormData();
+  formData.append("file", image);
+  formData.append("signature", signature);
+  formData.append("timestamp", timestamp.toString());
+  formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_KEY ?? "");
+
+  const response = await fetch(url, {
+    method: "post",
+    body: formData,
+  });
+
+  return response.json();
+}
 
 interface IFormData {
   address: string;
@@ -28,14 +62,14 @@ interface IProps {}
 export default function HouseForm({}: IProps) {
   const [submitting, setSubmitting] = useState(false);
   const [previewImage, setPreviewImage] = useState<string>();
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    errors,
-    watch,
-  } = useForm<IFormData>({ defaultValues: {} });
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
 
+  const address = watch("address");
+  const [createSignature] = useMutation<CreateSignatureMutation>(
+    SIGNATURE_MUTATION
+  );
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" });
     register({ name: "latitude" }, { required: true, min: -90, max: 90 });
@@ -43,7 +77,11 @@ export default function HouseForm({}: IProps) {
   }, [register]);
 
   const handleCreate = async (data: IFormData) => {
-    console.log(data);
+    const { data: signatureData } = await createSignature();
+    if (signatureData) {
+      const { signature, timestamp } = signatureData.createImageSignature;
+      const imageData = await uploadImage(data.image[0], signature, timestamp);
+    }
   };
 
   const onSubmit = (data: IFormData) => {
@@ -70,78 +108,80 @@ export default function HouseForm({}: IProps) {
         {errors.address && <p>{errors.address.message}</p>}
       </div>
 
-      <>
-        <div className="mt-4">
-          <label
-            htmlFor="image"
-            className="block p-4 border-4 border-gray-600 border-dashed cursor-pointer"
-          >
-            Click to add image (16:9)
-          </label>
-          <input
-            id="image"
-            name="image"
-            type="file"
-            accept="image/*"
-            style={{ display: "none" }}
-            ref={register({
-              validate: (fileList: FileList) => {
-                if (fileList.length === 1) return true;
-                return "Please upload one file";
-              },
-            })}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              if (event?.target?.files?.[0]) {
-                const file = event.target.files[0];
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                  setPreviewImage(reader.result as string);
-                };
-                reader.readAsDataURL(file);
-              }
-            }}
-          />
-          {previewImage && (
-            <img
-              src={previewImage}
-              className="object-cover mt-4"
-              style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+      {address && (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="block p-4 border-4 border-gray-600 border-dashed cursor-pointer"
+            >
+              Click to add image (16:9)
+            </label>
+            <input
+              id="image"
+              name="image"
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) return true;
+                  return "Please upload one file";
+                },
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
             />
-          )}
-          {errors.image && <p>{errors.image.message}</p>}
-        </div>
+            {previewImage && (
+              <img
+                src={previewImage}
+                className="object-cover mt-4"
+                style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
 
-        <div className="mt-4">
-          <label htmlFor="bedrooms" className="block">
-            Beds
-          </label>
-          <input
-            id="bedrooms"
-            name="bedrooms"
-            type="number"
-            className="p-2"
-            ref={register({
-              required: "Please enter the number of bedrooms",
-              max: { value: 10, message: "Wooahh, too big of a house" },
-              min: { value: 1, message: "Must have at least 1 bedroom" },
-            })}
-          />
-          {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
-        </div>
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              type="number"
+              className="p-2"
+              ref={register({
+                required: "Please enter the number of bedrooms",
+                max: { value: 10, message: "Wooahh, too big of a house" },
+                min: { value: 1, message: "Must have at least 1 bedroom" },
+              })}
+            />
+            {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
+          </div>
 
-        <div className="mt-4">
-          <button
-            className="px-4 py-2 font-bold bg-blue-500 rounded hover:bg-blue-700"
-            type="submit"
-            disabled={submitting}
-          >
-            Save
-          </button>{" "}
-          <Link href="/">
-            <a>Cancel</a>
-          </Link>
-        </div>
-      </>
+          <div className="mt-4">
+            <button
+              className="px-4 py-2 font-bold bg-blue-500 rounded hover:bg-blue-700"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>{" "}
+            <Link href="/">
+              <a>Cancel</a>
+            </Link>
+          </div>
+        </>
+      )}
     </form>
   );
 }
