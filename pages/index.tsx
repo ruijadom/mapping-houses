bb85e663fc03abde3b987f1c2e41eaf28c835cad
ruijadom <ruijadomingues@gmail.com>
2021-04-03 14:32:52 +0100

    query houses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
 // import { useState } from "react";
-// import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
 // import HouseList from "src/components/houseList";
-// import { useLastData } from "src/utils/useLastData";
+import { useLastData } from "src/utils/useLastData";
 import { useLocalState } from "src/utils/useLocalState";
-// import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
+import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
+
+const HOUSES_QUERY = gql`
+  query HousesQuery($bounds: BoundsInput!) {
+    houses(bounds: $bounds) {
+      id
+      latitude
+      longitude
+      address
+      publicId
+      bedrooms
+    }
+  }
+`;
 
 type BoundsArray = [[number, number], [number, number]];
+
+const parseBounds = (boundsString: string) => {
+  const bounds = JSON.parse(boundsString) as BoundsArray;
+
+  return {
+    sw: { latitude: bounds[0][1], longitude: bounds[0][0] },
+    ne: { latitude: bounds[1][1], longitude: bounds[1][0] },
+  };
+};
 
 export default function Home() {
   const [dataBounds, setDataBounds] = useLocalState<string>(
@@ -17,6 +39,16 @@ export default function Home() {
   );
 
   const [debounceDataBounds] = useDebounce(dataBounds, 200);
+  const { data, error } = useQuery<HousesQuery, HousesQueryVariables>(
+    HOUSES_QUERY,
+    {
+      variables: { bounds: parseBounds(debounceDataBounds) },
+    }
+  );
+
+  const lastData = useLastData(data);
+
+  if (error) return <Layout main={<div>Error loading houses</div>} />;
 
   return (
     <Layout
